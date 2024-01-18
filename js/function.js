60e94b715b15df7af5b1531fc6c8743268941cb7
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-01-18 15:36:07 +0100

    feat: risoluzione parte logica

--- js/function.js
+++ js/function.js
@@ -1,24 +1,43 @@
 // FUNCTIONS
 
-/* 
- @param {number} min il valore minimo da usare per la generazione
- @param {number} max il valore massimo da usare per la generazione
-
- @return Numero randomico generato tra minimo e massimo forniti
- 
+/**
+ *
+ * GENERA OPZIONI SELEZIONABILI IN UN ELEMENTO HTML
+ * @param {*} array array di opzioni selezionabili
+ * @param {*} input elemento html su cui applicarli
  */
-
-function getRandomNumber(min, max) {
-  min = parseInt(min);
-  max = parseInt(max);
-  if (isNaN(min) || isNaN(max)) {
-    console.error("i valori inseriti devono essere numerici");
-    return false;
+function addOptionToSelectInput(array, valueArray, input) {
+  for (let i = 0; i < array.length; i++) {
+    input.innerHTML += `<option value="${valueArray[i]}">${array[i]}</option>`;
   }
+}
 
-  if (min >= max) {
-    console.error("il valore minimo deve essere minore del valore massimo");
+/**
+ *
+ * @param {*} container elemento html che dovrà contenere le celle
+ * @param {*} cellNumber varia in base alla difficoltà scelta dall'utente
+ */
+function generateGrid(container, cellNumber) {
+  container.innerHTML = "";
+
+  for (let i = 0; i < cellNumber; i++) {
+    const cellEl = generateCell(i);
+    container.append(cellEl);
   }
-  const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
-  return randomNumber;
+}
+
+/**
+ *
+ * @param {*} i indice di numerazione delle celle
+ * @returns cella
+ */
+function generateCell(i) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell-" + difficultyInput.value);
+  cell.innerText = i + 1;
+  cell.addEventListener("click", function () {
+    this.classList.toggle("clicked");
+    console.log(cell.innerText);
+  });
+  return cell;
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-console.log("js ok");
+const gridContainerEl = document.querySelector(".grid-container");
+const difficultyInput = document.querySelector("#difficulty-level-input");
+const playButton = document.querySelector("#play-button");
 
-const resultElement = document.getElementById("results");
+gridContainerEl.innerHTML = "";
 
-getRandomNumber(1, 20);
-console.log(randomNumber);
+const difficultyLevel = ["easy", "medium", "hard"];
+const difficultyValueArray = [100, 81, 49];
+
+addOptionToSelectInput(difficultyLevel, difficultyValueArray, difficultyInput);
+
+playButton.addEventListener("click", function () {
+  const userDifficultyValue = difficultyInput.value;
+  generateGrid(gridContainerEl, userDifficultyValue);
+  console.log("cell-" + userDifficultyValue);
+});
